597ba037050e3e0270166de7b9757e168c42a89b
nick <shijh302@gmail.com>
2014-10-26 14:17:34 +0800

    角色脚本
    添加角色脚本

---
+++ Assets/Code/CharacterController2D.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	private const float SkinWidth = .02f;
+	private const int TotalHorizonRays = 8;
+	private const int TotalVerticalRays = 4;
+
+
+	private static readonly float SlopeLimitTarget = Mathf.Tan(75f * Mathf.Deg2Rad);
+
+	public LayerMask PlatformMask;
+	public ControllerParameters2D DefaultParameters;
+
+	public ControllerState2D state { get; private set; }
+
+	public void Awake()
+	{
+
+	}
+
+	public void AddForce(Vector2 force)
+	{
+
+	}
+
+	public void SetForce(Vector2 force)
+	{
+
+	}
+
+	public void SetHorizontalForce(float x)
+	{
+
+	}
+
+	public void SetVerticalForce(float x)
+	{
+		
+	}
+
+	public void Jump()
+	{
+
+	}
+
+	public void LateUpdate()
+	{
+
+	}
+
+	private void Move(Vector2 deltaMovement)
+	{
+
+	}
+
+	private void HandlePlatforms()
+	{
+
+	}
+
+	private void CaculateRayOrigins()
+	{
+
+	}
+
+	private void MoveHorizontally(ref Vector2 deltaMovement)
+	{
+
+	}
+
+	private void MoveVertically(ref Vector2 deltaMovement)
+	{
+		
+	}
+
+	private void HandleVerticalSlope(ref Vector2 deltaMovement)
+	{
+
+	}
+
+	private void HandleHorizontsSlope(ref Vector2 deltaMovement, float angle, bool isGoingHight)
+	{
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+
+	}
+
+	public void OnTriggerExit2D(Collider2D other)
+	{
+
+	}
+}
---
+++ Assets/Code/ControllerState2D.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerState2D
+{
+	public bool IsCollidingRight { get; set; }
+	public bool IsCollidingLeft { get; set; }
+	public bool IsCollidingAbove { get; set; }
+	public bool IsCollidingBelow { get; set; }
+	public bool IsMovingDownSlope { get; set; }
+	public bool IsMovingUpSlope { get; set; }
+	public bool IsGrounded {  get { return IsCollidingBelow; }  }
+	public float SlopeAngle { get; set;  }
+
+
+
+	public bool HasCollision { get {return IsCollidingRight || IsCollidingLeft || IsCollidingAbove || IsCollidingBelow;}}
+
+	public void Reset()
+	{
+		IsMovingUpSlope = 
+			IsMovingUpSlope = 
+				IsMovingDownSlope = 
+				IsCollidingBelow = 
+				IsCollidingAbove = 
+				IsCollidingLeft = 
+				IsCollidingRight = false;
+
+
+		SlopeAngle = 0;
+	}
+
+	public override string ToString ()
+	{
+		return string.Format (
+			"[ControllerState2D: IsCollidingRight={0}, IsCollidingLeft={1}, IsCollidingAbove={2}, IsCollidingBelow={3}, IsMovingDownSlope={4}, IsMovingUpSlope={5}, IsGrounded={6}, SlopeAngle={7}, HasCollision={8}]",
+			IsCollidingRight,
+			IsCollidingLeft, 
+			IsCollidingAbove, 
+			IsCollidingBelow, 
+			IsMovingDownSlope,
+			IsMovingUpSlope, 
+			IsGrounded, 
+			SlopeAngle, 
+			HasCollision);
+	}
+
+
+}
